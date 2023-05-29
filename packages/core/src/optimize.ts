36a1011895054f3f6249db7d1421f2c3a23ba1c0
cijiugechu <cijiugechu@gmail.com>
2023-05-29 11:47:28 +0800

    fix: ensure that optimize function has output

--- packages/core/src/optimize.ts
+++ packages/core/src/optimize.ts
@@ -1,5 +1,8 @@
 import { losslessCompressPng } from '@napi-rs/image'
 
-const optimize = (buf: Buffer) => losslessCompressPng(buf)
+const optimize = (buf: Buffer) =>
+  losslessCompressPng(buf, {
+    force: true,
+  })
 
 export { optimize }
